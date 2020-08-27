41a4a5d68a4f4cfe1e4acaea4ad3ff701c45e4b2
HitsujiRere <hitsujirere@gmail.com>
2020-08-27 11:20:58 +0900

    Add setting.csv.

--- shimayell_responce.py
+++ shimayell_responce.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
+import csv
+import pprint
+
+talks = {}
+with open(r'talks/setting.csv') as f:
+    reader = csv.reader(f)
+    l = [row for row in reader]
+    talks[l[0]] = l[1]
+
+
 end_texts = ['goodbye', 'bye-bye', 'ばいばい', 'バイバイ']
 def is_end_text(text):
     return text in end_texts
 
+
 def responce(text):
     if text == 'こんにちは':
         return 'こんにちは！'
-    
+
     return 'ごめんさい．分かりませんでした．'
